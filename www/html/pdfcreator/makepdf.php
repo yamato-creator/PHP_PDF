de7c7d4de0e8504dc23fbf4672846e53a728a7ad
yamato-creator <dydydydydydy5656@gmail.com>
2021-04-29 23:04:18 +0900

    fix

--- www/html/pdfcreator/makepdf.php
+++ www/html/pdfcreator/makepdf.php
@@ -2,14 +2,46 @@
 
 require('tfpdf/tfpdf.php');
 $pdf = new tFPDF;
-$pdf->AddPage();
 
 $pdf->AddFont('ShipporiMincho','','ShipporiMincho-Regular.ttf',true);
-$pdf->SetFont('ShipporiMincho','',14);
-$pdf->Cell(40,10,'1+1=');
- $pdf->Ln(10);
-$pdf->Cell(40,10,'1+2=');
- $pdf->Ln(10);
-$pdf->Cell(40,10,'1+3=');
+
+$names = htmlentities($_GET['names'], ENT_QUOTES, "utf-8");
+$names = explode("\r\n", $names);
+foreach($names as $name){
+  // ループ処理
+  $pdf->SetFont('ShipporiMincho','',20);
+  $pdf->AddPage();
+  $pdf->Cell(0,10,"たし算練習プリント");
+	$pdf->Ln(5);
+	$pdf->Cell(100);
+  $pdf->Cell(90,10,"名前 : $name","B");
+
+  $pdf->Ln(40);
+  make_contents();
+}
 
 $pdf->Output();
+
+function make_contents(){
+  global $pdf;
+  $contents = htmlentities($_GET['contents'], ENT_QUOTES, "utf-8");
+ 	$contents = explode("\r\n", $contents);
+ 	$count = 0;
+
+  $Y = $pdf->getY();
+
+  foreach($contents as $content){
+    $count ++;
+    if($count == 10){
+      $pdf->setY($Y);
+    }
+    if($count >= 10){
+      $pdf->setX(110);
+    }
+    $pdf->SetFont('ShipporiMincho','',25);
+ 		$pdf->Cell(20,10,"({$count})");
+ 		$pdf->SetFont('ShipporiMincho','',30);
+ 		$pdf->Cell(80,10,$content);
+ 		$pdf->Ln(25);
+  }
+}
